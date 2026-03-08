bfe185214e2c52227ba9ee70d165b2a243e951e2
MiriamDevora8156 <familyroberts19@gmail.com>
2026-03-08 10:32:42 +0200

    Update ServiceList.js

--- ServiceList.js
+++ ServiceList.js
@@ -7,7 +7,12 @@ function ServiceList() {
     // פנייה לשרת ה-Node החדש שהקמת
     fetch(`${process.env.REACT_APP_NODE_API_URL}/services`)
       .then(res => res.json())
-      .then(data => setServices(data))
+      //.then(data => setServices(data))
+      .then(data => {
+        // --- כאן הוספתי את ההדפסה ללוג ---
+        console.log("JSON response from Node server:", data); 
+        setServices(data);
+      })
       .catch(err => console.error("Error fetching services:", err));
   }, []);
 
